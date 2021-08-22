7be3ecdae8ea22b43411c759536ff5163799a79a
lilucha <lilybetancourt92@gmail.com>
2021-08-22 16:57:54 -0300

    Se agrega formulario del login, validacion para el campo vacio y boton cerrar sesion

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
+function login(event) {
+    event.preventDefault();
+    const username = document.getElementById('usuario').value;
+    const password = document.getElementById('contrasenia').value;
+
+    if (username == '' || password == '') {
+        alert('El usuario y la contraseña son requeridos');
+    } else {
+        window.location.href = 'home.html';
+    }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
 
 });
