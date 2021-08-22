4157a468fec82a6c07c1b49b8cb7398e51a613cc
lilucha <lilybetancourt92@gmail.com>
2021-08-22 18:12:48 -0300

    Se agrega imagen al login

--- js/products.js
+++ js/products.js
@@ -27,10 +27,10 @@ function showProductsList(productsArray) {
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-//document.addEventListener("DOMContentLoaded", function(e) {
-getJSONData(PRODUCTS_URL).then(function(resultObj) {
-    if (resultObj.status === "ok") {
-        showProductsList(resultObj.data);
-    }
+document.addEventListener("DOMContentLoaded", function(e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            showProductsList(resultObj.data);
+        }
+    });
 });
-//});
